e5cd2c397d2cbf33d55c745a5459e96eeb5ef431
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-03-03 16:04:20 -0600

    final

---
+++ todo.js
@@ -0,0 +1,29 @@
+let inputList = document.getElementById("newListInput");
+inputList.addEventListener("keyup",function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("addListButton").click();
+    this.value='';
+  }
+});
+
+
+function addList() {
+  let disp = document.getElementById("dispList"),
+      newLi = document.createElement("li");
+  newLi.innerHTML = '<input type="checkbox" /> ' + inputList.value;
+  disp.appendChild(newLi);
+
+  inputList.value = '';
+}
+
+function delList() {
+  let para = document.getElementById("dispList"),
+      checks = Array.prototype.slice.call(para.childNodes),
+      check;
+  while (check = checks.pop()) {
+    if(check.firstChild && check.firstChild.checked) {
+      para.removeChild(check);
+    }
+  }
+}
